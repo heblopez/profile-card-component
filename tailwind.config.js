060d48117d7a0b9cc41266c289d6eab9cf1c2fe4
Heberth López <heberth.lopez.19@gmail.com>
2024-09-15 15:26:27 -0500

    feat: Added config for Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+        colors: {
+            'blue': 'var(--blue)',
+            'eastern-blue': 'var(--eastern-blue)',
+            'red': 'var(--red)',
+            'white': 'var(--white)',
+            'very-light-grayish-blue': 'var(--very-light-grayish-blue)',
+            'light-grayish-blue-1': 'var(--light-grayish-blue-1)',
+            'light-grayish-blue-2': 'var(--light-grayish-blue-2)',
+            'grayish-blue': 'var(--grayish-blue)',
+            'dark-grayish-blue': 'var(--dark-grayish-blue)',
+            'very-dark-blue': 'var(--very-dark-blue)',
+        },
+    },
+  },
+  plugins: [],
+}
